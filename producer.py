c23a6ab4bf4f21e07c4f2ba8b72922a9d059fec6
Mario9810 <32363631+Mcoconan@users.noreply.github.com>
2021-11-11 16:32:08 -0600

    coomit

--- producer.py
+++ producer.py
@@ -11,12 +11,12 @@ producer = KafkaProducer(bootstrap_servers='20.120.14.159:9092')
 dirr = ["N", "NW", "W", "SW", "S", "SE", "E", "NE"]
 
 
-
-dic = {
-    "hum" : random.randrange(1,100),
-    "tem" : round(random.uniform(0.00, 99.99), 2),
-    "vin" : random.choice(dirr)
-}
-payload = json.dumps(dic).encode('utf-8')
-producer.send('17055',payload)
-time.sleep(15)
+while(True):
+    dic = {
+        "hum" : random.randrange(1,100),
+        "tem" : round(random.uniform(0.00, 99.99), 2),
+        "vin" : random.choice(dirr)
+    }
+    payload = json.dumps(dic).encode('utf-8')
+    producer.send('17055',payload)
+    time.sleep(15)
